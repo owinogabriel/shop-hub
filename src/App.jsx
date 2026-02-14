2dd124d3fae52110a30d20ab2675bf3d725136fe
owinogabriel <g73346920@gmail.com>
2026-02-14 11:19:56 +0300

    chore:Get auth functions and current user from context.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Auth from "./pages/Auth";
+import AuthProvider from "./context/AuthContext";
 
 function App() {
   return (
+<AuthProvider>
     <div className="app">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+         <Route path="/auth" element={<Auth />} />
       </Routes>
     </div>
+    </AuthProvider>
   );
 }
 
